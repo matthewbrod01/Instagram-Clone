83b9853ae3450be95e22df977d998743eac6c50d
Matthew <Matthew.b.rodriguez@gmail.com>
2018-09-30 20:24:03 -0400

    Initial Commit

---
+++ Instagram Clone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Instagram Clone
+//
+//  Created by Matthew on 9/30/18.
+//  Copyright © 2018 Matthew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
